bae4b53ed638b8e5e36d5083b9bcc270b0e27a58
root <pranayramteke613@gmail.com>
2025-08-14 20:05:59 +0530

    Added Code For Wrapper Classes

---
+++ src/section2_data_types_and_wrapper_class/Example2.java
@@ -0,0 +1,54 @@
+package section2_data_types_and_wrapper_class;
+
+public class Example2
+{
+
+    public static void main(String[] args)
+    {
+        /**
+         * byte       Byte
+         * short      Short
+         * int        Integer
+         * long       Long
+         *
+         * float      Float
+         * double     Double
+         *
+         * char       Character
+         *
+         * boolean    Boolean
+         *
+         */
+
+        System.out.println("Byte Min Value : " + Byte.MIN_VALUE);
+        System.out.println("Byte Max Value : " + Byte.MAX_VALUE);
+        System.out.println("Byte Size In Bits : " + Byte.SIZE);
+        System.out.println("Byte Size In Byte : " + Byte.BYTES);
+
+        System.out.println("Short Min Value : " + Short.MIN_VALUE);
+        System.out.println("Short Max Value : " + Short.MAX_VALUE);
+        System.out.println("Short SIZE In Bits : " + Short.SIZE);
+        System.out.println("Short SIZE In Bytes : " + Short.BYTES);
+
+        // Integer
+
+         // Long
+
+        // Float
+
+        // Double
+
+
+        System.out.println("Character Max Value : "+ Character.MAX_VALUE);
+        System.out.println("Character Min Value " + Character.MIN_VALUE);
+        System.out.println("Character Size : " + Character.SIZE);
+        System.out.println("Character Size Byte : " + Character.BYTES);
+
+        System.out.println("Boolean : " + Boolean.TRUE);
+        System.out.println("Boolean : " + Boolean.FALSE);
+
+
+
+
+    }
+}
